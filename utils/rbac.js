ebb31bb671d822ec2ac06f5dc84aba22b9b3c0a9
Irene Toxla <toxlairene@gmail.com>
2024-11-11 08:50:03 +0000

    added changes in todo endpoint

---
+++ utils/rbac.js
@@ -0,0 +1,13 @@
+export const permissions =[
+    {
+        role: "user",
+        actions: [
+            "get_profile",
+            "update_profile",
+            "add_habit",
+            "update_habit",
+            "get_one_habit",
+            "get_habits",
+        ]
+    }
+]
